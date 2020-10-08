02981eb24866b8b235ec58d021447252d7779008
alishazia <shaziaali812@gmail.com>
2020-10-08 17:17:20 +0530

    Using createRef for ref fuctionality

--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import './Person.css';
 
 class Person extends Component {
-    componentDidMount() {
-        this.inputElement.focus();
+    constructor(props) {
+        super(props);
+        this.inputElementRef = React.createRef();
     }
+    componentDidMount() {
+        this.inputElementRef.current.focus();
+    }
+    
     render() {
         return (
             <div className="Person" >
                 <p onClick={this.props.click}>I am {this.props.name} and i am {this.props.age}</p>
                 <p>{this.props.children}</p>
                 <input onChange={this.props.changed} value={this.props.name}
-                    ref={(inputEl) => { this.inputElement = inputEl }} />
+                    ref={this.inputElementRef} />
             </div>)
     }
 }
